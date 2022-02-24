7c5ec30754189a679a66a7648b0863443e952767
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-02-24 09:33:23 -0600

    Add Picker with segmented style Create static method in RecipeModel that contains calculating serving size

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -17,4 +17,25 @@ class RecipeModel: ObservableObject {
        
         self.recipes = DataService.getLocalData()
     }
+    
+    
+    static func getPortion(ingredient: Ingredient, recipeServings: Int, targetServings: Int) -> String {
+        
+        //Single serving size calculation
+        
+        
+        //Selected Serving size calculation
+        
+        
+        //Reduce franction by greatest common divisor
+        
+        
+        //Whole portion if num > dem
+        
+        
+        //Express the remainder as a fraction
+        
+        return String(targetServings)
+        
+    }
 }
--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     var recipe: Recipe
+    @State var selectedServingSize = 2
     
     var body: some View {
         
@@ -19,6 +20,18 @@ struct RecipeDetailView: View {
                         .resizable()
                         .scaledToFit()
                     
+                    //MARK: - Serving size picker
+                    VStack(alignment: .leading){
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 160)
+                    }
                     
                     VStack(alignment: .leading) {
                         Text("Ingredients")
@@ -26,7 +39,7 @@ struct RecipeDetailView: View {
                             .padding(.vertical, 5)
                         ForEach(recipe.ingredients){
                             item in
-                            Text("• " + item.name)
+                            Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name)
                             
                         }
                     }
